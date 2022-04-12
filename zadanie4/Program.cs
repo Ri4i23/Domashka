dc35fce56d2a3c10313500f2c91a68d7337d2686
Ruslan <ri4i23aminov@yandex.ru>
2022-04-12 20:44:22 +0300

    zadacha4

---
+++ zadanie4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задание 4
+Console.Write("Введите число = ");
+int a = Convert.ToInt32(Console.ReadLine());
+int OstDel = a%2;
+if (OstDel == 0)
+{
+    Console.WriteLine("Введенное число является четным ");
+}
+else
+{
+    Console.WriteLine("Введенное число является нечетным ");
+}
