f1e7348a38f1139f09695f56c55f73c137144527
Ruslan <ri4i23aminov@yandex.ru>
2022-04-12 20:21:58 +0300

    Sdelali vtruy zada4u

---
+++ zadanie2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2
+Console.Write("Введите первое число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("число a больше числа b ");
+}
+else
+{
+    Console.WriteLine("число b больше числа a ");
+}
