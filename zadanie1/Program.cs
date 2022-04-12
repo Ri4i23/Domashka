9f15df2e5e921965cf6ef52f63599324ec84fcaa
Ruslan <ri4i23aminov@yandex.ru>
2022-04-12 20:10:54 +0300

    Sdelali pervuy zada4u `

---
+++ zadanie1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задание 1
+Console.WriteLine("Введите трехзначное число");
+int a =Convert.ToInt32(Console.ReadLine());
+int b = a%10;
+Console.WriteLine("Последняя цифра числа ");
+Console.WriteLine(b);
