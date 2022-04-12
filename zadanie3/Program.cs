88cc7083f731ab124fd7eae487684731d0e738d0
Ruslan <ri4i23aminov@yandex.ru>
2022-04-12 20:33:15 +0300

    Zada4a3

---
+++ zadanie3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задание 3
+Console.Write("Введите первое число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write("Максимальное число = ");
+Console.Write(max);
