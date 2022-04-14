df535f1ccaa72b1284a1c7f9d7108981a45094fc
Ruslan <ri4i23aminov@yandex.ru>
2022-04-14 16:12:54 +0300

    zadanie5

---
+++ zadanie5/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задание 5
+Console.Write("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int number = 2;
+while (number <= N)
+{
+    Console.WriteLine(number);
+    number = number + 2;
+}
